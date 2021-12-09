6556b19c65cdbe1dae1bd1862ca191d3105010a0
BenjaminKoder <93762219+BenjaminKoder@users.noreply.github.com>
2021-12-09 10:14:38 +0100

    bugfixes

--- shyguy/game.js
+++ shyguy/game.js
@@ -1,6 +1,12 @@
 var character = document.getElementById("character")
 var block = document.getElementById("block")
 
+
+let currentDate = new Date();
+const tstart = currentDate.getTime();
+let score;
+
+
 function jump() {
     if(character.classList != "animate") {
     character.classList.add("animate"); 
@@ -18,7 +24,14 @@ var checkDead = setInterval(function(){
     if (blockLeft < 40 && blockLeft > 0 && characterTop>=260) {
         block.style.animation="none";
         block.style.display="none";
-        alert("Du tapte lol");
+        currentDate = new Date();
+
+        score = currentDate.getTime() - tstart
+        score = Math.floor(score/10)
+        console.log(score)
+        let msg = "Du tapte. Din score: " + score
+        //alert("Du tapte lol");
+        alert(msg);
     }
 
 }, 10)
