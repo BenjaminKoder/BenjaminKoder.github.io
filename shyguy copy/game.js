e39a4718150c59298c6b072de33ce98b23917b20
BenjaminKoder <93762219+BenjaminKoder@users.noreply.github.com>
2021-12-09 22:26:46 +0100

    fixed

---
+++ shyguy copy/game.js
@@ -0,0 +1,36 @@
+var character = document.getElementById("character")
+var block = document.getElementById("block")
+
+
+let currentDate = new Date();
+const tstart = currentDate.getTime();
+let score;
+
+
+function jump() {
+    if(character.classList != "animate") {
+    character.classList.add("animate"); 
+    }
+    setTimeout(function(){
+        character.classList.remove("animate");
+    }, 500)
+}
+
+var checkDead = setInterval(function(){
+    var characterTop=
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"))
+    var blockLeft=
+    parseInt(window.getComputedStyle(block).getPropertyValue("left"))
+    if (blockLeft < 40 && blockLeft > 0 && characterTop>=260) {
+        block.style.animation="none";
+        block.style.display="none";
+        currentDate = new Date();
+
+        score = currentDate.getTime() - tstart
+        score = Math.floor(score/10)
+        console.log(score)
+        let msg = "Du tapte lol. Din score er: " + score
+        alert(msg);
+    }
+
+}, 10)
